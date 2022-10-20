f6e4162cdba3416f2292152392a748ab23933781
antonio <ant.work.svirido@gmail.com>
2022-10-20 21:39:52 +0300

    Ассет плеер контролер, ovveride классов с ассета, настройка анимаций для игрока

---
+++ Assets/Game/Scripts/CharacterController/PersonController.cs
@@ -0,0 +1,12 @@
+﻿namespace Invector.vCharacterController
+{
+    public class PersonController : vThirdPersonController
+    {
+        private const string attackAnimation = "Attack";
+        
+        public void ControllAttack()
+        {
+            animator.SetTrigger(attackAnimation);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/CharacterController/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Invector.vCharacterController
+{
+    public class PlayerInput : vThirdPersonInput
+    {
+        public KeyCode attackInput = KeyCode.Mouse0;
+        private PersonController personController;
+        
+        protected override void Start()
+        {
+            base.Start();
+            personController = GetComponent<PersonController>();
+        }
+        protected override void InputHandle()
+        {
+            base.InputHandle();
+            AttackInput();
+        }
+
+        protected virtual void AttackInput()
+        {
+            if (Input.GetKeyDown(attackInput))
+            {
+                personController.ControllAttack();   
+            }
+        }
+    }
+}
